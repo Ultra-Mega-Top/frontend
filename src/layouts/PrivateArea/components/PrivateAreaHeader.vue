2fec2ea79283ceb8e8c3d8a7b00a518ea4b89285
Wendell Vieira <wendell.vieiracunha@gmail.com>
2023-07-07 19:46:47 -0300

    feat: add header of private area

---
+++ src/layouts/PrivateArea/components/PrivateAreaHeader.vue
@@ -0,0 +1,40 @@
+<template>
+	<header class="private-area-header">
+		<div class="identify bg-grey-2">
+			<q-list>
+				<q-item clickable v-ripple>
+					<q-item-section avatar>
+						<q-avatar color="primary" rounded text-color="white">
+							JD
+						</q-avatar>
+					</q-item-section>
+
+					<q-item-section>
+						<q-item-label>Jonh Due</q-item-label>
+						<q-item-label caption> Teachr </q-item-label>
+					</q-item-section>
+				</q-item>
+			</q-list>
+		</div>
+	</header>
+</template>
+<script lang="ts">
+	import { Component, Vue } from 'vue-property-decorator';
+
+	@Component
+	export default class PrivateAreaHeader extends Vue {}
+</script>
+<style lang="scss" scoped>
+	.private-area-header {
+		display: block;
+
+		.identify {
+			width: 300px;
+			float: right;
+			border: 1px solid $grey-5;
+			border-top: none;
+			border-bottom-right-radius: 4px;
+			border-bottom-left-radius: 4px;
+		}
+	}
+</style>
--- src/layouts/PrivateArea/index.vue
+++ src/layouts/PrivateArea/index.vue
@@ -1,16 +1,22 @@
 <template>
 	<div class="private-area">
+		<PrivateAreaHeader />
 		<router-view />
 	</div>
 </template>
 <script lang="ts">
 	import { Component, Vue } from 'vue-property-decorator';
 
-	@Component
+	import PrivateAreaHeader from './components/PrivateAreaHeader.vue';
+
+	@Component({ components: { PrivateAreaHeader } })
 	export default class PrivateArea extends Vue {}
 </script>
 <style lang="scss" scoped>
 	.private-area {
 		display: block;
+		width: 98%;
+		max-width: 1200px;
+		margin: 0 auto;
 	}
 </style>
