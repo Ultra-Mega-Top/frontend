fcc9685714684661ebdfcef230e04641b4a304b5
Wendell Vieira <wendell.vieiracunha@gmail.com>
2023-07-07 18:55:03 -0300

    feat: Remove unused files

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="q-app">
-    <router-view />
-  </div>
+	<div id="q-app">
+		<router-view />
+	</div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+	import { Vue, Component } from 'vue-property-decorator';
 
-@Component
-export default class App extends Vue {}
+	@Component
+	export default class App extends Vue {}
 </script>
