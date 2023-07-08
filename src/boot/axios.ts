120990b97583addf5f8fa7486d508c120d781888
Wendell Vieira <wendell.vieiracunha@gmail.com>
2023-07-08 16:18:14 -0300

    feat: setting axios

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -4,10 +4,17 @@ import { boot } from 'quasar/wrappers';
 declare module 'vue/types/vue' {
 	interface Vue {
 		$axios: AxiosInstance;
+		$api: AxiosInstance;
 	}
 }
+
+const api = axios.create({
+	baseURL: 'http://localhost:3000',
+});
 
 export default boot(({ Vue }) => {
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 	Vue.prototype.$axios = axios;
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+	Vue.prototype.$api = api;
 });
